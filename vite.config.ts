d90b1654d85485a88cf42642242884041f85eb90
송재욱 <haensol976520@gmail.com>
2025-01-25 05:28:44 +0900

    stop config

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://port-0-check-daily-carbon-emissions-server-gw1g2alq2jw8x4.sel5.cloudtype.app",
         changeOrigin: true,
       },
     },
