3be566b2c876fdd27db82c39a6a7e94f35a5112c
송재욱 <haensol976520@gmail.com>
2025-01-25 01:56:08 +0900

    o

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -4,16 +4,15 @@ import type { User } from "@/entities/user/model/types";
 import { axiosInstance } from "@/shared/api/axios";
 
 export const authApi = {
-  login: async (data: LoginForm): Promise<User> => {
-    const response = await axiosInstance.post("/login/", data);
-    return response.data;
-  },
-
-  register: async (data: RegisterForm): Promise<User> => {
-    const response = await axiosInstance.post("/register/", {
+  register: async (data: RegisterForm): Promise<void> => {
+    await axiosInstance.post("/register/", {
       username: data.username,
       password: data.password,
     });
+  },
+
+  login: async (data: LoginForm): Promise<User> => {
+    const response = await axiosInstance.post("/login/", data);
     return response.data;
   },
 };
